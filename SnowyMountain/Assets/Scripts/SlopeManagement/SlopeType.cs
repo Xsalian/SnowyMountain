3e5a2d7638c6657194438a508c8c34fbc7cced56
Krystian Szlagor <krystian.szlagor@4experience.co>
2023-03-06 08:21:12 +0100

    mapGenerator - Created SlopeType script

---
+++ SnowyMountain/Assets/Scripts/SlopeManagement/SlopeType.cs
@@ -0,0 +1,12 @@
+namespace SnowyMountain.SlopeManagment
+{
+    public enum SlopeType
+    {
+        DEFAULT,
+        LEFT_START_CLIFF,
+        LEFT_CLIFF,
+        RIGHT_START_CLIFF,
+        RIGHT_CLIFF,
+        TUNNEL
+    }
+}
